99c59fa54aba2ae5d61078cba5960eee8c7a2ed3
Tomasz Tracz <t@tracz.me>
2022-08-31 21:17:19 +0100

    Change feature file, add scenario for this project

---
+++ src/test/java/MyStepdefs.java
@@ -0,0 +1,13 @@
+import io.cucumber.java.en.Given;
+
+public class MyStepdefs {
+    @Given("an example scenario")
+    public void anExampleScenario() {
+
+    }
+
+    @Given("I am logged in")
+    public void iAmLoggedIn() {
+
+    }
+}
